170f99f4a3ce58e6ee9ee2606c940d01c3c654d2
digitalspy12 <shink0856@gmail.com>
2026-07-01 19:02:10 +0530

    chore: Rename app to Fall ak Smart Transfer

--- scripts/sync-version.js
+++ scripts/sync-version.js
@@ -30,7 +30,7 @@ fs.writeFileSync(cargoTomlPath, cargoToml)
 
 let readme = fs.readFileSync(readmePath, 'utf8')
 readme = readme.replace(/\/download\/v[\d.]+/g, `/download/v${version}`)
-readme = readme.replace(/AltSendme_[\d.]+_/g, `AltSendme_${version}_`)
+readme = readme.replace(/Fall-ak-Smart-Transfer_[\d.]+_/g, `Fall-ak-Smart-Transfer_${version}_`)
 readme = readme.replace(
 	/\[badge-version\]:\s*https:\/\/img\.shields\.io\/badge\/version-[\d.]+-blue/g,
 	`[badge-version]: https://img.shields.io/badge/version-${version}-blue`
--- web-app/src/lib/version.ts
+++ web-app/src/lib/version.ts
@@ -3,6 +3,6 @@ import packageJson from '../../../package.json'
 export const VERSION = packageJson.version
 export const VERSION_DISPLAY = `v${VERSION}`
 export const PRIVACY_LINK =
-	'https://github.com/fall-ak/smart-transfer/blob/main/PRIVACY.md'
+	'https://github.com/Digitalspy12/FalL-ak---smart-transfer-/blob/main/PRIVACY.md'
 export const LICENSE_LINK =
-	'https://github.com/fall-ak/smart-transfer/blob/main/LICENSE'
+	'https://github.com/Digitalspy12/FalL-ak---smart-transfer-/blob/main/LICENSE'
